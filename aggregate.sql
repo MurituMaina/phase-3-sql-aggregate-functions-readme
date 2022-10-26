aeb395308ed17ae3882a76c74364dc2ea555a34e
MurituMaina <vincent.maina@student.moringaschool.com>
2022-10-27 00:03:54 +0300

    add the learnt aggreagate functions

---
+++ aggregate.sql
@@ -0,0 +1,14 @@
+SELECT AVG(net_worth) FROM cats;
+
+
+SELECT SUM(net_worth) FROM cats
+
+
+
+SELECT MAX (net_worth) FROM cats;
+
+
+SELECT MIN (net_worth) FROM cats;
+
+
+ SELECT COUNT(*) FROM cats WHERE net_worth > 1000000;
